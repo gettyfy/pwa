10d141d5e211a45bf3bb02266b742a6f99a7e5d9
Andrew Miracle <me@andrewmiracle.com>
2020-03-03 02:40:34 +0000

    folder structure for apis

--- imports/startup/accounts-config.js
+++
@@ -1,5 +0,0 @@
-import { Accounts } from 'meteor/accounts-base';
-// console.log(Accounts.config)
-// Accounts.config({
-// 	passwordSignupFields: 'USERNAME_ONLY'
-// });
---
+++ imports/ui/components/Fields.tsx
@@ -0,0 +1,3 @@
+/**
+ * This File will house the Form Fields that would be re-used across the project
+ */
