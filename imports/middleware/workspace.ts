26ed79b00f19bc79113fd4e911662a27b9152125
Andrew Miracle <me@andrewmiracle.com>
2020-03-03 03:21:43 +0000

    middleware for workspace authorization

---
+++ imports/middleware/workspace.ts
@@ -0,0 +1,26 @@
+export const allowIsWorkspaceAdmin = function(userId, workspace) {
+	return workspace && workspace.hasAdmin(userId);
+};
+
+export const allowIsWorkspaceMember = function(userId, workspace) {
+	return workspace && workspace.hasMember(userId);
+};
+
+export const allowIsAnyWorkspaceMember = function(userId, workspaces) {
+	return _.some(workspaces, (workspace) => {
+		return workspace && workspace.hasMember(userId);
+	});
+};
+
+export const allowIsWorkspaceMemberCommentOnly = function(userId, workspace) {
+	return workspace && workspace.hasMember(userId) && !workspace.hasCommentOnly(userId);
+};
+
+export const allowIsWorkspaceMemberNoComments = function(userId, workspace) {
+	return workspace && workspace.hasMember(userId) && !workspace.hasNoComments(userId);
+};
+
+export const allowIsWorkspaceMemberByCard = function(userId, card) {
+	const workspace = card.workspace();
+	return workspace && workspace.hasMember(userId);
+};
