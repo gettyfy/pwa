b5d329dd8af83424de40bf4820fbef121c605383
Lady Dzidzor <ladydzidzor@gmail.com>
2020-03-04 20:12:59 +0000

    added import and export customerlist components in index.tsx/components

--- imports/ui/components/index.tsx
+++ imports/ui/components/index.tsx
@@ -1,4 +1,6 @@
 import ActionCard, { ActionCardRow } from './ActionCard'
+import CustomerList, { TransactionList } from './List'
 
 
 export { ActionCard, ActionCardRow}
+export { CustomerList, TransactionList}
