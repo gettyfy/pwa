c8a3ee874224f92900c164d10a62e7e2155beec9
Lady Dzidzor <ladydzidzor@gmail.com>
2020-03-05 15:56:10 +0000

    wip..added heading and channel boxes

--- imports/ui/pages/workspace/remind/index.tsx
+++ imports/ui/pages/workspace/remind/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Icon, Box } from "@chakra-ui/core";
+import { Icon, Box, Stack, Heading, Flex } from "@chakra-ui/core";
 import styled from '@emotion/styled'
 
 
@@ -16,6 +16,33 @@ export default class record extends React.Component {
         <Box>
           <Icon name="arrow-back" size="24px" />
         </Box>
+        <Stack spacing={3}>
+         <Heading as="h1" size="2xl">
+          Create a Reminder
+         </Heading>
+         <Box mt="6">
+          <h6>Select Channels</h6>
+          </Box>
+          </Stack>
+
+            {/* Channels */}
+          <Flex flexDirection="row" mt="10px" justify="space-between">
+        <Box border="1px" borderRadius="md" borderColor="yellow" w="16%" h="59px">
+          Card
+          </Box>
+          <Box border="1px" borderRadius="md" borderColor="blue" w="16%" h="59px">
+            Card
+          </Box>
+          <Box border="1px" borderRadius="md" borderColor="blue" w="16%" h="59px">
+            Card
+          </Box>
+          <Box border="1px" borderRadius="md" borderColor="blue" w="16%" h="59px">
+            Card
+          </Box>
+          <Box border="1px" borderRadius="md" borderColor="blue" w="16%" h="59px">
+            Card
+          </Box>
+        </Flex>
       </StyledRecord>
 
 
