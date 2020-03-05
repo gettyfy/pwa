9c3b8b33ed45f496d8d6277d0d495d3e8be9e90d
Andrew Miracle <me@andrewmiracle.com>
2020-03-05 12:47:52 +0000

    simplify component imports

--- imports/ui/components/List.tsx
+++ imports/ui/components/List.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from '@emotion/styled'
 import { Link } from 'react-router-dom'
-import { Box, Stack,Avatar, Text,Icon,Flex } from '@chakra-ui/core'
+import { Box, Stack, Avatar, Text, Icon, Flex } from '@chakra-ui/core'
 import * as Analytics from '/imports/ui/analytics';
 
 
@@ -29,9 +29,9 @@ interface ITransactionList {
 
 
 
-const TransactionList= (props: ITransactionList) => {
-    const { analyticName,boxPadding, customerProfile,customerName, customerStatus, amount,paymentStatus,overdueAmount,overdueStatus,iconName, iconSize, cardLink} = props
-    
+const TransactionList = (props: ITransactionList) => {
+    const { analyticName, boxPadding, customerProfile, customerName, customerStatus, amount, paymentStatus, overdueAmount, overdueStatus, iconName, iconSize, cardLink } = props
+
     const handleClick = (analyticName: string): any => {
 
         // /we will write the handle analytics here
@@ -40,50 +40,50 @@ const TransactionList= (props: ITransactionList) => {
         })
     }
 
-    return(
+    return (
         <Flex>
             {/* <Box d="flex"> */}
             {/* <Link to={cardLink}> */}
-                 <Box p={boxPadding}>
-                 <Stack isInline>
-                  <Avatar name={customerProfile} src="/" />
-                </Stack>
-                 </Box>
-
-                <Box>
-                    <Text fontSize="xs">{customerStatus}</Text>
-                  <Text fontSize="xs">{customerName}</Text>
-                </Box>
-
-                <Box p={boxPadding}>
-                    <Text fontSize="xs">{amount}</Text>
-                    <Text fontSize="xs">{paymentStatus}</Text>
-                </Box>
-
-                <Box p={boxPadding}>
-                    <Text fontSize="xs">{overdueAmount}</Text>
-                    <Text fontSize="xs">{overdueStatus}</Text>
-
-                </Box>
-
-                <Box p={boxPadding} onClick={handleClick(analyticName)}>
+            <Box p={boxPadding}>
                 <Stack isInline>
-                    <Link to={cardLink}> 
-                      <Icon name= {iconName} size={iconSize} />
-                      </Link>
-                    </Stack>
-                    
-                </Box>
-                
-                {/* </Link> */}
-                {/* </Box> */}
-              </Flex>
+                    <Avatar name={customerProfile} src="/" />
+                </Stack>
+            </Box>
 
-               
+            <Box>
+                <Text fontSize="xs">{customerStatus}</Text>
+                <Text fontSize="xs">{customerName}</Text>
+            </Box>
+
+            <Box p={boxPadding}>
+                <Text fontSize="xs">{amount}</Text>
+                <Text fontSize="xs">{paymentStatus}</Text>
+            </Box>
+
+            <Box p={boxPadding}>
+                <Text fontSize="xs">{overdueAmount}</Text>
+                <Text fontSize="xs">{overdueStatus}</Text>
+
+            </Box>
+
+            <Box p={boxPadding} onClick={handleClick(analyticName)}>
+                <Stack isInline>
+                    <Link to={cardLink}>
+                        <Icon name={iconName} size={iconSize} />
+                    </Link>
+                </Stack>
+
+            </Box>
+
+            {/* </Link> */}
+            {/* </Box> */}
+        </Flex>
+
+
 
     )
 }
 
 // export the components as modules to be resuable by other component
-export default CustomerList
-export {TransactionList}
+// export default CustomerList
+export { TransactionList, CustomerList }
