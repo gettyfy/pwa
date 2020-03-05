4b3ae105165dbc59e59466f6845b20e602754f2e
Lady Dzidzor <ladydzidzor@gmail.com>
2020-03-05 22:56:05 +0000

    wip.. Included customer list components to page, as well as adding the heading and some icons

--- imports/ui/pages/workspace/recover/index.tsx
+++ imports/ui/pages/workspace/recover/index.tsx
@@ -1,5 +1,124 @@
 import React from 'react'
+import { Box, Stack, Heading, Icon, Divider, Flex, Text} from '@chakra-ui/core'
+import styled from '@emotion/styled'
+import { CustomerList, TransactionList} from '/imports/ui/components'
 
-const Recover = () => <h1>Recover Me</h1>
+const StyledRecover = styled.main`
+  display: flex;
+  flex-direction: column;
+`
 
-export default Recover
+export default class recover extends React.Component{
+  render(){
+    return(
+        <StyledRecover>
+        <Box>
+          <Icon name="arrow-back" size="24px" />
+        </Box>
+        <Stack spacing={3}>
+          <Heading as="h1" size="2xl">
+            Recover
+         </Heading>
+          <Box mt="6">
+            <h6>Escalate a bad debt to a professional debt collector</h6>
+          </Box>
+          </Stack>
+
+          <Flex  mt="40px">
+          <Box mt="3">
+            <Icon name="search" size="13px" />
+        
+          </Box>
+
+          </Flex>
+
+        <Flex FlexDirection="row" justify="space-between" mt="6">
+         <Text fontSize="xs">Payment overdue</Text>
+          <Icon name="drag-handle" size="13px" ml="20"/>
+    
+          {/* <Box justify="space-between">
+            <Divider borderColor="grey.200" />
+          </Box> */}
+        </Flex>
+
+        <TransactionList
+
+          boxPadding="2"
+          customerProfile="Sasuke Uchiha"
+          customerStatus="10 days to overdue"
+          customerName="Evans Boateng"
+          amount="GHc233"
+          paymentStatus="PAID"
+          overdueAmount="GHC346"
+          overdueStatus="OVERDUE"
+          cardLink="/signup"
+          iconName="chevron-right"
+          iconSize="24px"
+        />
+        
+        <TransactionList
+           boxPadding="2"
+          customerProfile="Sasuke Uchiha"
+          customerStatus="10 days to overdue"
+          customerName="Evans Boateng"
+          amount="GHc233"
+          paymentStatus="PAID"
+          overdueAmount="GHC346"
+          overdueStatus="OVERDUE"
+          cardLink="/signup"
+          iconName="chevron-right"
+          iconSize="24px"
+        />
+        
+
+        <TransactionList
+         boxPadding="2"
+          customerProfile="Sasuke Uchiha"
+          customerStatus="10 days to overdue"
+          customerName="Evans Boateng"
+          amount="GHc233"
+          paymentStatus="PAID"
+          overdueAmount="GHC346"
+          overdueStatus="OVERDUE"
+          cardLink="/signup"
+          iconName="chevron-right"
+          iconSize="24px"
+        />
+        
+
+        <TransactionList
+        boxPadding="2"
+          customerProfile="Sasuke Uchiha"
+          customerStatus="10 days to overdue"
+          customerName="Evans Boateng"
+          amount="GHc233"
+          paymentStatus="PAID"
+          overdueAmount="GHC346"
+          overdueStatus="OVERDUE"
+          cardLink="/signup"
+          iconName="chevron-right"
+          iconSize="24px"
+        />
+        
+
+        <TransactionList
+          boxPadding="2"
+          customerProfile="Sasuke Uchiha"
+          customerStatus="10 days to overdue"
+          customerName="Evans Boateng"
+          amount="GHc233"
+          paymentStatus="PAID"
+          overdueAmount="GHC346"
+          overdueStatus="OVERDUE"
+          cardLink="/signup"
+          iconName="chevron-right"
+          iconSize="24px"
+        />
+
+
+
+        </StyledRecover>
+
+    )
+  }
+}
