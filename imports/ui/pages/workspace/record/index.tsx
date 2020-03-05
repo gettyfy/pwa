3c5eaa3b6ad868fee860adae00e3474a53553102
Andrew Miracle <me@andrewmiracle.com>
2020-03-05 14:00:32 +0000

    create workspace for clients

---
+++ imports/ui/pages/workspace/record/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Icon, Box } from "@chakra-ui/core";
+import styled from '@emotion/styled'
+
+
+const StyledRecord = styled.main`
+  display: flex;
+  flex-direction: column;
+`
+
+
+export default class record extends React.Component {
+  render() {
+    return (
+      <StyledRecord>
+        <Box>
+          <Icon name="arrow-back" size="24px" />
+        </Box>
+      </StyledRecord>
+
+
+    )
+  }
+}
